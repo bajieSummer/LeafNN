abac80a2c7f661e75e5bf12a57379f297daf5e32
Sophie <bajie615@163.com>
2024-11-09 10:26:42 +0800

    add plots, and use np.set_printoptions

--- LeafNNPython/demos/TestBaseClassifyModel.py
+++ LeafNNPython/demos/TestBaseClassifyModel.py
@@ -1,5 +1,6 @@
-import numpy as np
 import demoInit
+import LeafNN.core.Bases.MathMatrix
+import numpy as np
 import os
 from LeafNN.utils.PathUtils import PathUtils 
 from LeafNN.utils.Log import Log
@@ -7,6 +8,7 @@ from LeafNN.utils.DataUtils import DataUtils as DUtils
 from LeafNN.core.LeafModels.BaseClassifyModel import BaseClassifyModel as BCM
 from LeafNN.core.LeafModels.NeuralLeaf import NeuralLeaf
 import LeafNN.core.LeafModels.TrainMonitor as TMot
+from LeafNN.utils.ModelVisualizer import ModelVisualizer as MV
 LeafBaseTestTag = "LeafBaseModel_Test"
 
 def readData1():
@@ -21,6 +23,9 @@ def testReadSimpleData():
     XY = np.hstack([X,Y])
     Log.Debug(LeafBaseTestTag,f"X shape={X.shape}, Y shape={Y.shape}")
     Log.Debug(LeafBaseTestTag,f"\n x=>\n{XY}")
+    MV.plotData(X,Y)
+
+    
 
 def testPredictXBeforeTrain():
     # test case1: with ex2data1.txt
@@ -32,13 +37,16 @@ def testPredictXBeforeTrain():
     X = data.X
     Y = data.Y
     Log.Debug(LeafBaseTestTag,f"X shape={X.shape}, Y shape={Y.shape}")
-    wb_mats =[np.array([[-24.0],[0.2],[0.2]])]
+    wb_mats =[np.array([[0.0],[0.0],[0.0]])]# -24.0 0.2,0.2
     wb = NeuralLeaf(wb_mats)
     (xn,xm) = X.shape
     model1 = BCM([xm,1],wb)
     Y_p = model1.predict(X,wb)
     XYY_p = np.hstack([X,Y,Y_p])
     Log.Debug(LeafBaseTestTag,f" predict with certain values \n,{XYY_p}")
+    indsStart = 2
+    indsEnd = 10
+    MV.plotDataWithTestCase(X,Y,X[indsStart:indsEnd],Y[indsStart:indsEnd],Y_p[indsStart:indsEnd])
 
 def testCalCostAndGrad():
     # test case1:
@@ -64,11 +72,12 @@ def testCalCostAndGrad():
 
 def testTrain():
     data = readData1()
-    wb_mats =[np.array([[100.0],[100.0],[100.0]])]
+    MV.plotData(data.X,data.Y,"All Datas")
+    wb_mats =[np.array([[1000.0],[1000.0],[1000.0]])]
     wb = NeuralLeaf(wb_mats)
     (xn,xm) = data.X.shape
     model1 = BCM([xm,1],wb)
-    model1.trainOption.MaxIteration = 100
+    model1.trainOption.MaxIteration = 300
     model1.trainOption.trainRatio = 1.0
     model1.trainOption.validationRatio = 0.0
     model1.trainOption.testRatio = 0.0
@@ -78,18 +87,33 @@ def testTrain():
     monitorData=TMot.MonitorData()
     model1.train(monitorOpiton,monitorData)
     newWb = model1.wb
+    MV.plotCostWithWB(monitorData.iterationInds,monitorData.costs,wb,newWb)
+    startInds = 2
+    endInds = 10
+    testX = data.X[startInds:endInds]
+    testY = data.Y[startInds:endInds]
+    Y_p=model1.predict(testX,model1.wb)
+    MV.plot2DDecisionBoundaryWithTestCase(newWb,data.X,data.Y,testX,testY,Y_p)
+    # more debug info
     Log.Debug(LeafBaseTestTag,f"afterTrain: newWb=\n {newWb}")
     finalTrainCost = model1.calCost(newWb,model1.trainData)
     finalValidCost = model1.calCost(newWb,model1.validateData)
     finalTestCost = model1.calCost(newWb,model1.testData)
     Log.Debug(LeafBaseTestTag,f"afterTrain: trainCost={finalTrainCost},validCost={finalValidCost},testCost={finalTestCost}")
     Log.Debug(LeafBaseTestTag,f"monitorData costs=\n{monitorData.costs} \nrates=\n{monitorData.rates},")
-
-
+np.set_printoptions(precision=20, suppress=True)
 def main():
     Log.Debug(LeafBaseTestTag,"test case run")
+
     #testReadSimpleData()
     #testPredictXBeforeTrain()
     #testCalCostAndGrad()
     testTrain()
+
+    #data visual flow-> 
+    # before train: dataXY --> 
+    # train: train Costs/Iteration.(how many iterations, early stop?) -->
+    #  after train: train_dataXY_Y_p, decisionBoundary,(train:pass rate.)-->
+    
+
 main()
