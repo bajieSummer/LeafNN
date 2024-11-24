30988c0766d50fee7ee1f3be4380135898a6c268
Sophie <bajie615@163.com>
2024-11-24 23:05:37 +0800

    Test cases: case5TrainAll2All case6predictTrainDataWithWB_all2all case7predictMyHandWritesWithWB_all2all

--- LeafNNPython/demos/TestHandWritesClassify.py
+++ LeafNNPython/demos/TestHandWritesClassify.py
@@ -53,33 +53,20 @@ def createImgMatsFromData(X,ShowWidth=1024,isTranspose=False,pic_width=None):
     return imgsMat
     
     
-def displayImgsFromX(X,ShowWidth=1024,imgWidth=None):
-    imgMats = createImgMatsFromData(X,ShowWidth,imgWidth)
+def displayImgsFromX(X,ShowWidth=1024,isTranspose=False,imgWidth=None):
+    imgMats = createImgMatsFromData(X,ShowWidth,isTranspose,imgWidth)
     img = Image.fromarray(imgMats*255)
     osize = img.size
     scaled_image = img.resize((osize[0]*2,osize[1]*2), Image.LANCZOS) 
     scaled_image.show()
-
-def transposeMatX(X):
-    [m,n] = X.shape
-    picW = int(MM.sqrt(n))
-    resX = MM.ones(X.shape)
-    for i in range(m):
-        rxi = X[i,:].reshape([picW,int(n/picW)])
-        resX[i,:] = MM.transpose(rxi).flatten()
-    return resX
+    return scaled_image
 
 
-
-def readMatData1(file_name,transpose=False):
+def readMatData1(file_name,transpose=False,picW=None):
     #file_name = "ex3data1.mat"
     dataPath = os.path.join(PathUtils.getDemoDatasPath(),file_name)
-    data = DUtils.readDataXYFromFile(dataPath)
-    X = data.X
-    X = transposeMatX(X)
-    Y = data.Y
-    Log.Debug(HandWriteClassifyTestTag,f"datafirstpic: ==\n{X[0,:]}")
-    return MD.ClassifyData(X,Y)
+    data = DUtils.readDataXYFromFile(dataPath,transpose,picW)
+    return data
 
 def readWB(file_name):
     dataPath = os.path.join(PathUtils.getDemoDatasPath(),file_name)
@@ -217,17 +204,17 @@ def case3TestHandWritingsWithTrainedWB():
     displayImgsFromX(tX[xindices,:],400)
     MV.plotYYpre(Y_indices,tY[xindices,:],YRes,f"predict")
 
-def createTestXYFromBaseNumberPics(folderPath,isResize=False,picW=None,picH=None):
+def createTestXYFromBaseNumberPics(folderPath,isTranspose = False,isResize=False,picW=None,picH=None):
     directory = Path(folderPath)
-    file_paths = list(directory.rglob('*'))
+    file_paths = list(directory.rglob('*.jpg'))
     testX = None
     testY = None
     for fpath in file_paths:
         image = Image.open(fpath)
         if image is None:
             continue
-        if(isResize and picW and picH):
-            image = image.resize((picW,picH))
+        # if(isResize and picW and picH):
+        #     image = image.resize((picW,picH))
         image = image.convert('L')
         filename = os.path.basename(fpath)
         numbers = re.findall(r'\d+', filename)
@@ -237,7 +224,8 @@ def createTestXYFromBaseNumberPics(folderPath,isResize=False,picW=None,picH=None
         else:
             continue
         image_array = MM.array(image)
-        #image_array = MM.transpose(MM.array(image))
+        if isTranspose:
+            image_array = MM.transpose(MM.array(image))
         Log.Debug(HandWriteClassifyTestTag,f"img=\n{image_array}")
         image_array = image_array.flatten().reshape(1, -1)
         # to do why
@@ -257,7 +245,7 @@ def case4TestPredictNumbersFromPics():
     #createTestXYFromPics(folderPath)
     folder_name = "testPics1"
     testdataFolderPath = os.path.join(PathUtils.getDemoDatasPath(),folder_name)
-    testXY = createTestXYFromBaseNumberPics(testdataFolderPath,True,20,20)
+    testXY = createTestXYFromBaseNumberPics(testdataFolderPath,False,True,20,20)
     numLabels = 10
     wbs=[]
     for i in range(numLabels):
@@ -271,7 +259,158 @@ def case4TestPredictNumbersFromPics():
     MV.plotYYpre(xindices,testXY.Y,YRes,f"numberPicsPredict,size={size_y}")
     Log.Debug(HandWriteClassifyTestTag,f"y-y_pre-probilitys\n{MM.hstack([testXY.Y,YRes,Yprob])}")
 
+# def transposeX(X,picW,picH):
+#     [m,n] = X.shape
+#     Xres = X
+#     for i in range(m):
+#         xi = X[i,:].reshape([picW,picH])
+#         xi = MM.transpose(xi)
+#         Xres[i,:] = xi.flatten()
+#     return Xres
+
+def case5TrainAll2All():
+    origData = readMatData1("ex3data1.mat",True)
+    # processY
+    X = origData.X
+    oY = origData.Y
+    [m,n] = oY.shape
+    oY[oY==10] = 0
+    numLabels = 10
+    Y = MM.zeros([m,numLabels])
+    for i in range(m):
+        yi = int(oY[i,0].flatten())
+        Y[i,yi] = 1
+    # inital wb
+    [m,n] = X.shape
+    [m,n2] = Y.shape
+    wb_mats_init =[]
+    initV = 0.0
+    layerSizeList = [n,25,n2]
+    for l in range(len(layerSizeList)-1):
+        wb_mats_init.append(MM.ones([layerSizeList[l]+1,layerSizeList[l+1]])*initV)
+    wb_init = NeuralLeaf(wb_mats_init)
+    # createModels
+    model1 = BCM(layerSizeList)
+    model1.trainOption.MaxIteration = 100
+    model1.trainOption.trainRatio = 1.0
+    model1.trainOption.validationRatio = 0.0
+    model1.trainOption.testRatio = 0.0
+    model1.trainOption.regularLamada = 10.0
+    model1.setData(MD.ClassifyData(X,Y))
+    monitorOpiton=TMot.MonitorOption()
+    monitorOpiton.enable = True
+    monitorData=TMot.MonitorData()
+    model1.train(monitorOpiton,monitorData)
+    MV.plotCostWithWB(monitorData.iterationInds,monitorData.costs,wb_init,model1.wb)
+    saveWB1(model1.wb,"wbs_ex3data1.json")
+    MV.plotGradientsSquare(monitorData.iterationInds,monitorData.grads,"Gradients Strength Trend in Training")
+
+    YRes = model1.predict(X,model1.wb)
+    YRes_process = MM.argmax(YRes,1)
+    passedNums =0
+    for i in range(m):
+        if(YRes_process[i]==oY[i]):
+            passedNums+=1
+    passRate = passedNums/m
+    Log.Debug(HandWriteClassifyTestTag,f"samples={m},passRate={passRate}")
+    #  
+import numpy as np
+def saveImgs(folderPath,X,Y,picW,picH):
+    [m,n] = X.shape
+    # os.path.join(PathUtils.getDemoDatasPath()
+    if(picW*picH)!=n:
+        Log.Error(HandWriteClassifyTestTag,"wrong picw,and pich")
+        return
+    for i in range(m):
+        ix = X[i,:].reshape([picW,picH])*255.0
+        #ix = ix.astype(np.uint8)
+        img = Image.fromarray(ix)
         
+        filePath = os.path.join(folderPath,f"Y{Y[i,0]}_x{i}.jpg")
+        #img.save(filePath)
+        #img.show()
+        img_converted = img.convert('L') 
+        #img_converted.show()
+        img_converted.save(filePath)
+
+def case6predictTrainDataWithWB_all2all():
+    origData = readMatData1("ex3data1.mat",True)
+    wb = readWB("wbs_ex3data1.json")
+    outSide = False
+    # origData = readMatData1("ex3data1.mat",False)
+    # wb = readWB("ex3weights.mat")
+    # outSide = True
+    indStart =4600
+    indEnd = 4700
+    X = origData.X[indStart:indEnd,:]
+    Y = origData.Y[indStart:indEnd,:]
+    Y[Y==10] = 0
+    #Log.Debug(HandWriteClassifyTestTag,f"Y=\n{Y}")
+    numLabels = 10
+    [m,n] = X.shape
+    [m,n2] = Y.shape
+    numLabels = 10
+    
+    layerSizeList=[n,25,numLabels]
+    model1 = BCM(layerSizeList,wb)
+    YRes = model1.predict(X,wb)
+    Log.Debug(HandWriteClassifyTestTag,f"YRes=\n{YRes}")
+    YRes_process = MM.argmax(YRes,1).reshape(-1,1)
+    if(outSide):
+        YRes_process = YRes_process+1
+        YRes_process[YRes_process == 10] = 0
+    YCompare = MM.hstack([Y,YRes_process])*1.0
+    Log.Debug(HandWriteClassifyTestTag,f"Y-YRes_process=\n{YCompare}")
+    passedNums =0
+    for i in range(m):
+        if(YRes_process[i]==Y[i]):
+            passedNums+=1
+    displayImgsFromX(X,400)
+    folderPath = os.path.join(PathUtils.getDemoDatasPath(),"savePics")
+    saveImgs(folderPath,X,Y,20,20)
+    passRate = passedNums/m
+    Log.Debug(HandWriteClassifyTestTag,f"samples={m},passRate={passRate}")
+
+
+def case7predictMyHandWritesWithWB_all2all():
+    folder_name = "testPics1"
+    # wb = readWB("ex3weights.mat")
+    # outSide = True
+    wb = readWB("wbs_ex3data1.json")
+    outSide =False
+    
+    testdataFolderPath = os.path.join(PathUtils.getDemoDatasPath(),folder_name)
+    originTestXY = createTestXYFromBaseNumberPics(testdataFolderPath,outSide,False,20,20)
+    X = originTestXY.X
+    oY = originTestXY.Y
+    numLabels = 10
+    [m,n] = X.shape
+    [m,n2] = oY.shape
+    #oY[oY==10] = 0
+    Y = MM.zeros([m,numLabels])
+    for i in range(m):
+        yi = int(oY[i,0].flatten())
+        Y[i,yi] = 1
+    # inital wb
+   
+    layerSizeList=[n,25,numLabels]
+    model1 = BCM(layerSizeList,wb)
+    YRes = model1.predict(X,wb)
+    YRes_process = MM.argmax(YRes,1).reshape(m,1)
+    if(outSide):
+        YRes_process = YRes_process+1
+        YRes_process[YRes_process == 10] = 0
+    passedNums =0
+    for i in range(m):
+        if(YRes_process[i]==originTestXY.Y[i]):
+            passedNums+=1
+    passRate = passedNums/m
+    Log.Debug(HandWriteClassifyTestTag,f"samples={m},passRate={passRate}")
+    displayImgsFromX(X,400)
+    indices = MM.arange(m)
+    MV.plotYYpre(indices,originTestXY.Y,YRes_process,"img-numbers identify")
+    Log.Debug(HandWriteClassifyTestTag,f"YResProcess-Y{MM.hstack([YRes_process,oY])}")
+
      
 MM.set_printoptions(20,True)
 def main():
@@ -279,6 +418,9 @@ def main():
     #Case1showData()
     #case2TrainOneVsAll()
     #case3TestHandWritingsWithTrainedWB()
-    case4TestPredictNumbersFromPics()
+    #case4TestPredictNumbersFromPics()
+    #case5TrainAll2All()
+    #case6predictTrainDataWithWB_all2all()
+    case7predictMyHandWritesWithWB_all2all()
 
 main()
