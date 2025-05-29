78dd4740109e1e8f14cbb86babf6310797c80c29
Sophie <bajie615@163.com>
2025-05-29 17:18:01 +0800

    Adjust algorithm

--- LeafNNPython/LeafNN/ConvexOptimizer/NewtonIteration.py
+++ LeafNNPython/LeafNN/ConvexOptimizer/NewtonIteration.py
@@ -5,7 +5,7 @@ from LeafNN.Bases.MatrixLinear import MatrixLinear as ML
 from .LineSearcher import ArmijoWolfeLineSearcher
 NewtonMsgTag = "NewtonIteration"
 class NewtonIteration:
-    def __init__(self,calFFunc,calFAndGradient,maxIteration=200,epslion =1e-15,skipGrad0Eps=1e-3):
+    def __init__(self,calFFunc,calFAndGradient,maxIteration=200,epslion =1e-20,skipGrad0Eps=1e-5):
         Log.Info(NewtonMsgTag,"createNewtonIteration")
         if calFFunc is None:
             Log.Error(NewtonMsgTag,"Invalid Parameter, the calFFunc is None")
@@ -20,6 +20,7 @@ class NewtonIteration:
         self.skipGrad0Eps = skipGrad0Eps
         self.defaultLineSh = ArmijoWolfeLineSearcher(calFFunc,calFAndGradient)
         self.hessianDamp = self.epslion*self.epslion
+        self.lamadaK = 1.0 -skipGrad0Eps
         
     def calD(gradient,gradientSquare,fx):
         #d*gradient = -fx
@@ -56,8 +57,6 @@ class NewtonIteration:
         gradient = None
         while iterNum < self.maxIteration:
             (fx,gradient) = self.calFuncAndGradient(X,*FuncGradArgs)
-            checkfx = self.calFFunc(X,*FuncGradArgs)
-            Log.Debug(NewtonMsgTag,f"fx={fx},checkfx={checkfx}")
             # f'(xk)(xk+1 - xk) + f(xk) = 0
             # xk+1 = xk - f(xk)/f'(xk)
             # d = -f(xk)/f'(xk)
@@ -85,8 +84,47 @@ class NewtonIteration:
         #if detH ==0:
         #t = -abs(fx)/gradientSquare
         #d = t*gradient
+        isDetH0 = math.isclose(detH,0.0,abs_tol=self.epslion)
+        isGradient0 =  math.isclose(gradientSquare,0.0,abs_tol=self.epslion*self.epslion)
+        isf0 = math.isclose(fx,0.0,abs_tol=self.skipGrad0Eps)
+       
+        # if isDetH0:
+        #     if not isGradient0:
+        #         # means exist gradient0 on some direction but can escape from other direction
+        #         if isf0:
+        #             t = 1.0/MM.sqrt(gradientSquare)
+        #             #t = min(1.0/MM.sqrt(gradientSquare),1e10) # accelerate
+        #             #d = -1.0*t*gradient # take a try might oscillate
+        #             d = -1.0*gradient*t
+        #         else: 
+        #             t = min(abs(fx)/gradientSquare,1e10) # less than line search math.pow(2,-20)
+        #             #t = min(abs(fx)/gradientSquare,1.0/MM.sqrt(gradientSquare))
+        #             #t = min(t,1e10)
+        #             d = -t*gradient # more stable
+        #             #d = -abs(fx)/gradientSquare*gradient
+        #     else:
+        #         # flat area. graident0 and cure0
+        #         #d = -1.0*gradient/np.sqrt(gradientSquare)
+        #         H_d = HessMatrix +MM.identity(HessMatrix.shape[0])*self.hessianDamp
+        #         d = abs(ML.getInverse(H_d))@gradient*(-1.0)
+        # else:
+        #     d = abs(ML.getInverse(HessMatrix))@gradient*(-1.0)
+        H_d = HessMatrix
+        if isDetH0:
+            H_d = HessMatrix +MM.identity(HessMatrix.shape[0])*self.hessianDamp
+            #find proper H_d
+            det_H_d = ML.det(H_d)
+            if math.isclose(det_H_d,0.0,abs_tol=self.epslion*self.epslion):
+                Log.Debug(NewtonMsgTag,f"calMinD>>,isDetH0={isDetH0},det_H_d = {det_H_d}")
+                H_d = HessMatrix +MM.identity(HessMatrix.shape[0])*1e-16
+       
+        d = abs(ML.getInverse(H_d))@gradient*(-1.0)
+        Log.Debug(NewtonMsgTag,f"calMinD,isDetH0={isDetH0},isGradient0={isGradient0},isf0={isf0},detH={detH},hessianM={HessMatrix},d={d}")
+        # return d 
+
+            # 
         # if math.isclose(detH,0.0,abs_tol=self.epslion):
-        #     #d = -1.0*gradient
+        # #     #d = -1.0*gradient
         #     if math.isclose(fx,0.0,abs_tol=self.skipGrad0Eps): # avoid when f(x)=0, but not the minimum
         #         d =-1.0*gradient #-1.0/gradientSquare*gradient
         #         Log.Debug(NewtonMsgTag,f"fx_close to 0,fx={detH},d=\n{d},gradient={gradient}\n")
@@ -94,8 +132,9 @@ class NewtonIteration:
         #         d = -abs(fx)/gradientSquare*gradient # more stable
         #     Log.Debug(NewtonMsgTag,f"detH close to 0,detH={detH},d=\n{d},HessMatrix={HessMatrix}")
         # else:
-        H_d = HessMatrix + MM.identity(HessMatrix.shape[0])*self.hessianDamp
-        d = abs(ML.getInverse(H_d))@gradient*(-1.0)
+        #     H_d = HessMatrix + MM.identity(HessMatrix.shape[0])*self.hessianDamp*2
+        #     #min(1, 0.5/abs(fx))
+        #     d = abs(ML.getInverse(H_d))@gradient*(-1.0)
         return d
 
     def calMin(self,initX,calHessianFunc,*FuncGradArgs,customLineSearcher=None,histDataCollector=None):
@@ -140,23 +179,28 @@ class NewtonIteration:
                 # detH <0 and grad ==0,   # normal saddle point z=x^2-y^2
                 # detH~0, and grad = 0, might flat area or degenerated saddle point z=x^4-y^4
                 Log.Debug(NewtonMsgTag,f"try find the min-> iterNum={iterNum},detH={detH},X={X},fx={fx},grad=\n{gradient}\n,HesssianMatrix=\n{hessM}\n,lastd={lastd}\n")
-                if(detH<=self.skipGrad0Eps):#self.epslion):# skipGrad0Eps
-                    Log.Debug(NewtonMsgTag,f"saddle or other situation")
-                    if lastd is None:
-                         X = X + self.skipGrad0Eps
-                    else:
-                        dt=lastd*1.0/(math.sqrt(lastd.T@lastd))
-                        X = X + self.skipGrad0Eps*dt# self.epslion
-                    iterNum+=1
-                    continue
-                Log.Debug(NewtonMsgTag,f"try find the min-> succeed-min")
-                return (X,fx,gradient)
+                
+                if(fx == -1.0*math.inf): # already to the least
+                    return (X,fx,gradient)
+                
+                # if(detH<=self.skipGrad0Eps):#self.epslion):# skipGrad0Eps
+                #     Log.Debug(NewtonMsgTag,f"saddle or other situation")
+                #     if lastd is None:
+                #          X = X + self.skipGrad0Eps
+                #     else:
+                #         dt=lastd*1.0/(math.sqrt(lastd.T@lastd))
+                #         X = X + self.skipGrad0Eps*dt# self.epslion
+                #     iterNum+=1
+                #     continue
+                # Log.Debug(NewtonMsgTag,f"try find the min-> succeed-min")
+                # return (X,fx,gradient)
             #d = -fx/gradient
             d = self.calMinD(gradient,gradientSquare,fx,hessM,detH)
-            Log.Debug(NewtonMsgTag,f"iterNum={iterNum},X={X},fx={fx},d={d},grad=\n{gradient}\n,HesssianMatrix=\n{hessM}")
+            Log.Debug(NewtonMsgTag,f"iterNum={iterNum},X={X},fx={fx},detH={detH},d={d},grad=\n{gradient}\n,HesssianMatrix=\n{hessM}")
             alpha = lineSh.lineSearchMin(X,d,fx,gradient,*FuncGradArgs)
-            lambda_k =1.0# min(1, 0.5/abs(fx))
-            X = X +d*(lambda_k*alpha)
+            # min(1, 0.5/abs(fx))
+            X = X +d*alpha
+            Log.Debug(NewtonMsgTag,f"iterNum={iterNum}-->,newX={X} oldX={X-d*alpha} d={d},alpha={alpha}")
             iterNum+=1
             lastd = d
            
